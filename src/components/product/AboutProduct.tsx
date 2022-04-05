021e12c621c367dd53364e8a8fa801d7d8224206
sy <greyhairchooselife@gmail.com>
2022-04-06 08:23:59 +0900

    In an useEffect with empty [], let axios call api to get products, suppliers.

--- src/components/product/AboutProduct.tsx
+++ src/components/product/AboutProduct.tsx
@@ -18,7 +18,66 @@ type Props = {
 	setRootMode: Dispatch<SetStateAction<IRootMode>>,
 }
 
+interface ISupplierInfo {
+	id: number,
+	name: string,
+	address: string,
+	contact: string,
+	note?: string,
+	margin_ratio?: string | number,
+}
+
+interface IProductInfo {
+	id: number,
+	product_code: string,
+	name: string,
+	size: string,
+	registered_date: Date,
+	purchased_cost: number,
+	supplier_id: number,
+	barcode: string,
+}
+
 export const AboutProduct = ({ rootMode, setRootMode }: Props) => {
+	//********************************************************************
+	//		generate useState
+	//********************************************************************
+	const [reRender, setReRender] = useState([]);					//just for rerendering
+	const [supplier, setSupplier] = useState<ISupplierInfo[] | null>(null);
+	const [product, setProduct] = useState<IProductInfo[] | null>(null);
+	
+	
+	//********************************************************************
+	//		get init supplier && re-render as suppliers updated
+	//********************************************************************
+	useEffect(() => {
+		const fetchSuppliers = async () => {
+			try{
+				const response = await api.get('/supplier/get');
+				setSupplier(response.data);
+			} catch (err: any){
+				throw err;
+			}
+		};
+
+		const fetchProduct = async () => {
+			try{
+				const response = await api.get(`/product/get/${rootMode.productId}`);
+					setProduct(response.data);
+			} catch (err: any){
+				throw err;
+			}
+		};
+
+		fetchSuppliers();
+		fetchProduct();
+	}, []);
+
+	useEffect(() => {
+	}, []);
+	
+	console.log(product);
+
 
 	return (
 		<div>hi, I am about Product component. I am pointing {rootMode.productId}</div>
